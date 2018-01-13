4dfe022070e6e6618e019ff028e457c3ef0576ab
TsjipTsjip <victor.van.herel@telenet.be>
2018-01-13 11:14:06 +0100

    Update and rename test.js to example-command.js

---
+++ commands/example-command.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
+  // INSERT COMMAND ACTIONS HERE
+};
+
+exports.conf = {
+  enabled: false,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "example",
+  category: "Miscellaneous",
+  description: "A standard issue example command.",
+  usage: ";example"
+};
